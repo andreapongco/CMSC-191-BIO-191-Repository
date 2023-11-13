b03f4c60b794b33c58b566c274c2dd26c7d4d160
andreapongco <aspongco@up.edu.ph>
2023-11-13 11:51:14 +0800

    Saved id, modified date, and random columns into the table2.csv file

--- table1.py
+++ table1.py
@@ -1,9 +1,11 @@
 import pandas as pd
+import random
 
 df = pd.read_csv('table1.csv')
 
 df.insert(0, "id", range(1, len(df)+1))
 df['Date'] = "META-" + df['Date']
+df.insert(2, "Random", random.randint(0, 2))
 
 
-df.to_csv('table1.csv', index=False)
+df.to_csv('table2.csv', index=False)
