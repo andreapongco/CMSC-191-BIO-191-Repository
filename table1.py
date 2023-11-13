bc2a0bad562f2173cb4ef2c29f4d20eefbb49647
andreapongco <aspongco@up.edu.ph>
2023-11-13 12:02:40 +0800

    Modified the code for generating random numbers in the Random column

--- table1.py
+++ table1.py
@@ -5,7 +5,7 @@ df = pd.read_csv('table1.csv')
 
 df.insert(0, "id", range(1, len(df)+1))
 df['Date'] = "META-" + df['Date']
-df.insert(2, "Random", random.randint(0, 2))
+df.insert(2, "Random", [random.randint(0, 1) for i in range(0, len(df))])
 
 
 df.to_csv('table2.csv', index=False)
