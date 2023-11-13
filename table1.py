5ee3f772823e55ea57841a2d3d7f48b9118f9aed
andreapongco <aspongco@up.edu.ph>
2023-11-13 11:24:53 +0800

    Added id column as the first column

--- table1.py
+++ table1.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 df = pd.read_csv('table1.csv')
 
-df['id'] = range(1, len(df)+1)
+#df['id'] = range(1, len(df)+1)
+
+df.insert(0, "id'", range(1, len(df)+1)) 
 
 df.to_csv('table1.csv', index=False)
